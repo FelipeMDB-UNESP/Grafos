875eec34deac7207cc85d7c0aa87891aaa3d8759
FelipeMDB <>
2024-08-28 14:13:35 -0300

    Update Djikistra.c

--- Djikistra.c
+++ Djikistra.c
@@ -21,35 +21,72 @@ int** inicializa_matriz(int qtd_vertices){
 
 
 
-void djikstra(Grafo g, int** matriz_distancias, int qtd_vertices){
-
-    int i, j = 0;
+int* djikstra(int** matriz_distancias, int qtd_vertices, int origem) {
+    int* vetor_distancias = (int*)malloc(sizeof(int) * qtd_vertices);
     int* vetor_visitados = (int*)malloc(sizeof(int) * qtd_vertices);
-    for(i = 0; i < qtd_vertices; i++){
+    int i, j;
+
+    // Inicializa todos os vértices como não visitados e as distâncias como maior número possível
+    for (i = 0; i < qtd_vertices; i++) {
+        vetor_distancias[i] = INT_MAX;
         vetor_visitados[i] = 0;
     }
-    
-    matriz_distancias[i][j] = 0;
-    for(int i = 0; i < qtd_vertices; i++){
-        matriz_distancias[1][i] = 
+
+    // A distância da origem para si mesma é 0
+    vetor_distancias[origem] = 0;
+
+    //percorre todos os vértices
+    for (i = 0; i < qtd_vertices - 1; i++) {
+        int min_dist = INT_MAX;
+        int u = -1;
+
+        // Encontra o vértice não visitado com a menor distância
+        for (j = 0; j < qtd_vertices; j++) {
+            if (!vetor_visitados[j] && vetor_distancias[j] < min_dist) {
+                min_dist = vetor_distancias[j];
+                u = j;
+            }
+        }
+
+        // Marca o vértice atual como visitado
+        vetor_visitados[u] = 1;
+
+        // Atualiza as distâncias dos vértices adjacentes
+        for (j = 0; j < qtd_vertices; j++) {
+            //se o vértice j não foi visitado, existe um caminho de u para j e se a distância uj é menor que a distância atual guardada
+            if (!vetor_visitados[j] && matriz_distancias[u][j] && vetor_distancias[u] != INT_MAX &&
+                vetor_distancias[u] + matriz_distancias[u][j] < vetor_distancias[j]) {
+                vetor_distancias[j] = vetor_distancias[u] + matriz_distancias[u][j];
+            }
+        }
     }
 
-}
+    // Libera o vetor de visitados
+    free(vetor_visitados);
 
-void floyd(int** matriz_distancias){
-
-
+    // Retorna o vetor de distâncias
+    return vetor_distancias;
 }
 
 
 int main(){
     Grafo g;
-    int qtd_vertices;
+    int qtd_vertices, origem;
     printf("Qual a quantidade de vertices?\n");
     scanf(" %d", &qtd_vertices);
+    
+    printf("Qual a origem? O ponto de onde se pretende sair?\n");
+    scanf(" %d", &origem);
 
     int** matriz_distancias = inicializa_matriz(qtd_vertices);
-    djikstra(g, matriz_distancias, qtd_vertices);
-    floyd(matriz_distancias);
+    int* distancias = djikstra(g, matriz_distancias, qtd_vertices, origem);
+    
+    if (distancias != NULL) {
+        // Exibe as distâncias da origem para todos os vértices
+        for (int i = 0; i < qtd_vertices; i++) {
+            printf("Distância do vértice %d até %d: %d\n", origem, i, distancias[i]);
+        }
+        free(distancias);
+    }
 
 }
