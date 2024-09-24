045c84cd7bc1fb2607cdd7e1931488fdccf1921d
FelipeMDB <>
2024-09-24 17:03:41 -0300

    Update arvore3.c
    adição de bfs

--- Trabalho1/arvore3.c
+++ Trabalho1/arvore3.c
@@ -16,6 +16,41 @@ Vitor Marchini Rolisola
 
 #define ITERACOES 10
 
+#pragma region fila
+
+typedef struct {
+    int* data;
+    int entrada, saida, tamanho;
+} Fila;
+
+Fila* inicializarFila(int max_size) {
+    Fila* f = (Fila*)malloc(sizeof(Fila));
+    f->data = (int*)malloc(max_size * sizeof(int));
+    f->entrada = 0;
+    f->saida = 0;
+    f->tamanho = max_size;
+    return f;
+}
+
+void enfileirar(Fila* f, int value) {
+    if (f->saida < f->tamanho) {
+        f->data[f->saida++] = value;
+    }
+}
+
+int desenfileirar(Fila* f) {
+    if (f->entrada < f->saida) {
+        return f->data[f->entrada++];
+    }
+    return -1; // Fila vazia
+}
+
+int estaVazia(Fila* f) {
+    return f->entrada == f->saida;
+}
+
+#pragma endregion fila
+
 #pragma region log
 int contador_logs = 1;
 
@@ -231,12 +266,14 @@ int gerar_arvore(int* arvore, float prob, int profundidade) {
             continue;
         }
 
-        if (rand() % 100 < (int)(prob*100) ? true : false){
+        if (camada < profundidade) {
+            if (rand() % 100 < (int)(prob*100) ? true : false){
 
-            indice = 1 + ((qtd_filhos + 1) * qtd_noh);
-            arvore[i] = indice;
-            arvore[indice] = (i/(qtd_filhos+1))*(qtd_filhos+1) + 1;
-            qtd_noh++;
+                indice = 1 + ((qtd_filhos + 1) * qtd_noh);
+                arvore[i] = indice;
+                arvore[indice] = (i/(qtd_filhos+1))*(qtd_filhos+1) + 1;
+                qtd_noh++;
+            }
         }
 
         fprintf(log,"|%d", arvore[i]);
@@ -245,6 +282,7 @@ int gerar_arvore(int* arvore, float prob, int profundidade) {
     fechar_log(log);
     return qtd_noh;
 }
+
 
 #pragma endregion arvore
 
@@ -346,6 +384,98 @@ float media_geral_parametros(int qtd_filhos, float prob, int profundidade) {
     return media_geral/ITERACOES;
 }
 
+#pragma region bfs_iterativa
+int bfs_arvore_binomial (int *arvore, int raiz, int qtd_nos) {
+
+    // Vetor para armazenar as profundidades de cada nó
+    float *profundidades = (float *) malloc(qtd_nos * sizeof(float));
+    //precisávamos iterar em profundidades de forma sequencial, essa é a maneira mais simples;
+    int pos_profundidades = 0;
+    //Marca os nós visitados
+    int *visitados = (int *) calloc(qtd_nos, sizeof(int));
+    // Inicializa todas as profundidades com 0
+    for (int i = 0; i < qtd_nos; i++) {
+        profundidades[i] = 0;
+    }
+
+    //um bloco é representado pelo pai + filhos. Ou seja podemos iterar sobre os nós do vetor da árvore indo de bloco em bloco. 
+    int bloco = arvore[0] + 1;
+    //fila de índices
+    Fila *fila = inicializarFila(qtd_nos);
+
+    //profundidade inicial é zero
+    profundidades[raiz / bloco] = 0;
+    enfileirar(fila, raiz);
+    visitados[(raiz / bloco)] = 1;
+
+    int no_anterior;
+    while(!estaVazia(fila)) {
+
+        int no_atual = desenfileirar(fila);
+        visitados[no_atual / 4] = 1;
+        // printf("Visitando no: %d\n", (no_atual / bloco) + 1); // Visita o nó atual
+
+        //i começa em 1 para podermos usar essa variável para encontrarmos o filho na posição correta, ignorando a info do pai.
+        //no_atual - 1 -> para posicionarmos no começo das informações do nó, não em seu fim.
+        //multiplicamos por bloco pois cada bloco representa um nó de forma abstrata.
+        //soma-se i+1 no final pois queremos salvar os dados dos filhos. A primeira posição do bloco sempre armazena o índice de onde começa o bloco do pai do nó atual.
+        int pai_ou_filho;
+        for(int i = 0; i < bloco; i++) {
+
+            pai_ou_filho = arvore[no_atual + i];
+            if (pai_ou_filho != 0 && !visitados[(pai_ou_filho / bloco)]) {
+                enfileirar(fila, pai_ou_filho);
+
+                profundidades[pai_ou_filho / bloco] = profundidades[no_atual / bloco] + 1;
+            }
+        }
+    } 
+
+    // Calcula a média das profundidades
+    int maior_profundidade = 0;
+    for(int i = 0; i < qtd_nos; i++) {
+        if(profundidades[i] > maior_profundidade)
+            maior_profundidade = profundidades[i];
+    }
+
+    // Libera a memória da fila e do vetor de profundidades
+    free(fila->data);
+    free(fila);
+    free(profundidades);
+    // Retorna a profundidade média
+    return maior_profundidade;
+}
+
+float media_bfs_iterativa (int *arvore, int raiz, int qtd_nos, int bloco) {
+
+    float soma_profundidades;
+    // Chama a função para cada nó como raiz
+    //quantidade de filhos + 1 é como se fôssemos de índice a índice.
+    // no meio do for é bloco - 1
+    for (int raiz = 1; raiz <= ((qtd_nos-1) * bloco) + 1; raiz  = raiz + bloco) {
+        int profundidade = bfs_arvore_binomial(arvore, raiz, qtd_nos);
+        printf("Profundidade da arvore com raiz = noh %d: %d\n", (raiz/bloco)+1, profundidade);
+        soma_profundidades += (float)profundidade; // Acumula a soma das profundidades
+    }
+
+    return soma_profundidades / qtd_nos;
+}
+
+float britto_media_geral_parametros(int qtd_filhos, float prob, int profundidade) {
+
+    int* arvore = inicializar_arvore(qtd_filhos, prob, profundidade);
+    float media_geral = 0.0;
+
+    for (int i = 0; i<ITERACOES; i++) {
+        int qtd_nos = gerar_arvore(arvore, prob, profundidade);
+        media_geral += media_bfs_iterativa(arvore, 1, qtd_nos, qtd_filhos+1);
+    }
+
+    liberar_arvore(arvore);
+    return media_geral/ITERACOES;
+}
+#pragma endregion bfs_iterativa
+
 /*
 Função que solicita ao usuário o preenchimento de certas informações via prompt
 
@@ -413,7 +543,7 @@ int menu() {
     int opcao;
 
     do {
-        printf("\n\n+-------------------Menu---------------------+\n|1 - Inserir dados                           |\n|2 - Carregar dados                          |\n|3 - Ver média de profundidade dos parâmetros|\n|4 - Sair                                    |\n+--------------------------------------------+\n");
+        printf("\n\n+-------------------Menu---------------------+\n|1 - Inserir dados                           |\n|2 - Carregar dados                          |\n|3 - Ver media de profundidade dos parametros|\n|4 - Sair                                    |\n+--------------------------------------------+\n");
         scanf(" %d", &opcao);
     } while (opcao <1 || opcao > 4);
 
@@ -440,7 +570,7 @@ int main() {
             break;
         case 3:
             if (qtd_filhos != -1) {
-                printf("\nA média geral dos parâmetros é: %.2f", media_geral_parametros(qtd_filhos, prob, profundidade));
+                printf("\nA media geral dos parametros é: %.2f", britto_media_geral_parametros(qtd_filhos, prob, profundidade));
             } else {
                 printf("\nAdicione dados primeiro.\n");
             }
