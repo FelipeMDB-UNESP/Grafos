b885e1fd85088e3cc922d9c3d9a8ba8e27c6734d
Nogueira-A-L-D <105943852+Nogueira-A-L-D@users.noreply.github.com>
2024-09-24 11:35:43 -0300

    Separação e Upload das Funções

--- funcoes_grafos.c
+++ funcoes_grafos.c
@@ -1,43 +1,88 @@
+/*
+Por:
+André Luis Dias Nogueira
+Felipe Melchior de Britto
+Ryan Hideki Tadeo Guimarães
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 #include <time.h>
 #include <limits.h>
+#include <string.h>
 
+#pragma region matrizes
+
+/*
+Estrutura genérica de matriz. Criada com o intuito de construir um grafo.
+
+Objetos da estrutura:
+int n -> linhas.
+int ** matriz -> ponteiro para matriz.
+*/
 typedef struct matricial {
-    int n;     //linhas
+
+    int n;          //linhas
     int **matriz; //ponteiro para matriz
 }Matriz;
 
-Matriz* inicializar_matriz(int n){
+
+/*
+Função que inicializa uma matriz para grafos.
+
+Parâmetros:
+int qtd_vertices -> quantidade de vértices desejados.
+
+Retorno 
+ponteiro para a matriz inicializada.
+*/
+Matriz* inicializar_matriz(int qtd_vertices){
 
     Matriz* matricial = (Matriz*) malloc(sizeof(Matriz));
-    int** matriz_distancias = (int**) malloc(n * sizeof(int*));
-    for (int i = 0; i < n; i++) {
-        matriz_distancias[i] = (int*)malloc(n * sizeof(int));
+    int** matriz_adjascencia = (int**) malloc(qtd_vertices * sizeof(int*));
+    for (int i = 0; i < qtd_vertices; i++) {
+        matriz_adjascencia[i] = (int*)malloc(qtd_vertices * sizeof(int));
     }
-    matricial->n = n;
-    matricial->matriz = matriz_distancias;
+    matricial->n = qtd_vertices;
+    matricial->matriz = matriz_adjascencia;
     return matricial;
 }
 
+/*
+Função que libera o espaço dinamicamente alocado para uma matriz, incluindo todos os dados armazenados nela.
+
+Parâmetros:
+Matriz* matricial -> ponteiro para a matriz a ser liberada.
+*/
 void liberar_matriz(Matriz* matricial){
 
     for (int i = 0; i < matricial->n; i++) {
         free(matricial->matriz[i]);
     }
     free(matricial->matriz);
-    free(matricial->n);
     free(matricial);
 }
+#pragma endregion matrizes
 
-void gerar_grafo(Matriz* matricial, bool orientado, int prob) {
-    srand(time(NULL));
+#pragma region grafo_aleatorio
+/*
+Função para gerar um grafo com arestas aleatórias.
+
+Parâmetros:
+Matriz* matricial -> ponteiro de matriz para se gerar as arestas.
+bool orientado -> booleano que informa se a matriz é orientada ou não.
+int probabilidade -> probabilidade de cada aresta.
+*/
+void gerar_grafo(Matriz* matricial, bool orientado, int probabilidade) {
+    
+    
     if (!orientado) {   //garante espelhamento 
         for (int i = 0; i < matricial->n; i++) {
             for (int j = i; j < matricial->n; j++) {
                 if (i != j) {                          //evitar ligacoes proprias
-                    matricial->matriz[i][j] = (rand() % 100 < prob) ? 1  * (rand() % 10 + 1) : 0;//random de números entre 0 e 99 (resto da divisao por 100)
+                    srand(time(NULL));
+                    matricial->matriz[i][j] = (rand() % 100 < probabilidade) ? (rand() % 10 + 1) : 0;//random de números entre 0 e 99 (resto da divisao por 100)
                     matricial->matriz[j][i] = matricial->matriz[i][j];
                     
                 } else {
@@ -49,7 +94,7 @@ void gerar_grafo(Matriz* matricial, bool orientado, int prob) {
         for (int i = 0; i < matricial->n; i++) {
             for (int j = 0; j < matricial->n; j++) {
                 if (i != j) {                          //evitar ligacoes proprias
-                    matricial->matriz[i][j] = (rand() % 100 < prob) ? 1  * (rand() % 10 + 1) : 0;//random de números entre 0 e 99 (resto da divisao por 100)
+                    matricial->matriz[i][j] = (rand() % 100 < probabilidade) ? 1  * (rand() % 10 + 1) : 0;//random de números entre 0 e 99 (resto da divisao por 100)
                 } else {
                     matricial->matriz[i][j] = 0;       //falso para quando for a diagonal principal       
                 }
@@ -58,11 +103,4 @@ void gerar_grafo(Matriz* matricial, bool orientado, int prob) {
     }
 }
 
-int* criar_vetor(int n) {
-    int* vetor = (int*)malloc(sizeof(int) * n);
-    return vetor;
-}
-
-void liberar_vetor(int* vetor) {
-    free(vetor);
-}
+#pragma endregion grafo_aleatorio
---
+++ funcoes_vetores.c
@@ -0,0 +1,42 @@
+/*
+Por:
+André Luis Dias Nogueira
+Felipe Melchior de Britto
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
+#include <limits.h>
+#include <string.h>
+
+#pragma region vetores
+/*
+Função responsável por criar dinamincamente um vetor.
+
+Parâmetros:
+int n -> tamanho do vetor
+
+Retorno:
+int* vetor -> vetor dinamicamente alocado
+*/
+int* criar_vetor(int n) {
+
+    int* vetor = (int*)malloc(sizeof(int) * n);
+    return vetor;
+}
+
+/*
+Função responsável por liberar o espaço alocado a um vetor.
+
+Parâmetros:
+int* vetor -> ponteiro do vetor a ser liberado
+
+Retorno: void
+*/
+void liberar_vetor(int* vetor) {
+
+    free(vetor);
+}
+#pragma endregion vetores
